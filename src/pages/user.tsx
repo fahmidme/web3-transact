d6148e243b0f8095b7a7d076218eeb9034ba9f1f
fahmidme <fahmid.me@gmail.com>
2022-09-18 20:29:54 -0400

    User information displayed in a new page after a user connects wallet

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,13 +1,62 @@
 import { getSession, signOut } from 'next-auth/react';
 
+
 // gets a prop from getServerSideProps
 function User({ user }) {
+    const { address, profileId, signature } = user;
+
     return (
-        <div>
-            <h4>User session:</h4>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-            <button onClick={() => signOut({ redirect: '/' })}>Sign out</button>
+      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+          Web3 <span className="text-purple-500">Transact</span>
+        </h1>
+
+        <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg font-medium leading-6 text-gray-900">
+              Web3 Explorer Info 👽
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                {`Here's your verified-via-backend wallet info.`}
+            </p>
+          </div>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Wallet Address</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {address}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Profile Id
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {profileId}
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Signature
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  {signature}
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
+
+        <div className="mt-12">
+          <button
+            className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+            onClick={() => signOut({ redirect: "/" })}
+          >
+            Sign Out
+          </button>
+        </div>
+      </main>
     );
 }
 
